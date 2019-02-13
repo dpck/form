37fccde8902c7a8bcb8bd12da5b4dc2466158d75
Anton <anton@adc.sh>
2019-02-13 03:58:31 +0300

    add depack page

--- splendid/pages/index.js
+++ splendid/pages/index.js
@@ -4,7 +4,18 @@
 export const main = {
   title: '@Depack/form: Preact Bootstrap Form',
   url: 'index.html',
-  file: 'index',
+  file: '1-index',
   menuUrl: '/',
   menu: '@Depack/form',
+}
+
+/**
+ * @type {import('splendid').Page}
+ */
+export const compilation = {
+  title: 'Depack Compile Preact Bootstrap Form',
+  url: '∂epack-compile-preact-bootstrap-form.html',
+  file: '2-depack',
+  // menuUrl: '/depack',
+  menu: 'Compiled W/ Depack',
 }
